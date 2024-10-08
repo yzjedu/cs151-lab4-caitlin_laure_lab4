297c94c99b4f9859fc8f18739b85f31ed54d631e
caitlinburns <ceburns1@loyola.edu>
2024-10-08 16:41:42 -0400

    code and reflection

--- RD1.md
+++ RD1.md
@@ -1,6 +1,7 @@
 # Reflection Document
 
-* Drive Full Name  : 
-* Partner Full Name: 
-* Student ID:  
+* Drive Full Name  : Caitlin Burns
+* Partner Full Name: Laure Patera
+* Student ID:  1903398
+  * In this lab we were supposed to learn how to implement while statements in our code. We were also supposed to learn how to use f string formatting in out code. The steps we followed were to first work on our algorithm, then work on our test cases, then code, and finally make sure our code works and matches the algorithm. The key concepts explored were while statements, nested if statements, and f strings. The results did match what I expected to get, and we used the test cases to make sure our code was running properly.The main challeneg we ecnountered was the while statement not working properly. We solved that but changing all of the or statements to and statements.I do think I learned what i was supposed to learn in this lab and I enjoyed working with my partner. 
 
--- main.py
+++ main.py
@@ -1,4 +1,55 @@
-# Read the README.md
-# Read it again
-# Your code here
-# Delete these 4 lines of comments
+# Programmers:  Caitlin Burns and Laure Patera
+# Course:  CS151, Professor Zee
+# Due Date: 10/9/2024
+# Lab Assignment: 4
+# Problem Statement: this program tells a user how much they owe for their monthly phone bill
+# Data In: Package choice and data used
+# Data Out:  Total amount due
+# Credits: This code is based on the example in the read me file
+
+#this program tells a user how much they owe on their monthly phone bill
+
+package = input('What phone package do you have? ')
+package = package.lower()
+while package != 'green' and package != 'blue' and package != 'purple':
+    print('Invalid package. Please try again.')
+    package = input('What phone package do you have? ')
+    package = package.lower()
+
+#works if input is green
+if package == 'green':
+    price = 49.99
+    data = 2
+    data_used = input('How much data have you used this month?')
+    data_used = int(data_used)
+    if data_used > data:
+        additional_data = data_used - data
+        total_price = price + (additional_data * 15)
+    else:
+        total_price = price
+    coupon = input('Do you have a coupon? ')
+    if coupon == 'yes' and total_price > 75:
+        total_price = total_price -20
+    elif coupon == 'no':
+        total_price = total_price
+#works if input is blue
+elif package == 'blue':
+    price = 70.00
+    data = 4
+    data_used = input('How much data have you used this month?')
+    data_used = int(data_used)
+    if data_used > data:
+        additional_data = data_used - data
+        total_price = price + (additional_data * 10)
+    else:
+        total_price = price
+#works if input is purple
+elif package == 'purple':
+    price = 99.95
+    total_price = price
+
+print(f'You owe ${total_price:.2f}')
+
+
+
+
